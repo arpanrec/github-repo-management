3747c33a4212abc05ffa928a1b7feaa1ec4802d4
arpan <arpan.rec@outlook.com>
2022-01-29 19:34:07 +0530

    terraform_cloud_management

---
+++ repo-terraform-cloud-management.tf
@@ -0,0 +1,13 @@
+resource "github_repository" "terraform_cloud_management" {
+  name               = "terraform-cloud-management"
+  license_template   = var.license_template
+  description        = "Manage Terraform Cloud Workspaces"
+  visibility         = "public"
+  gitignore_template = "Terraform"
+  auto_init          = true
+}
+
+resource "github_branch_default" "terraform_cloud_management" {
+  repository = github_repository.terraform_cloud_management.name
+  branch     = var.repository_default_branch_01292022_main
+}
