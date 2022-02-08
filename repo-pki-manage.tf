e451442695c3ef62ce84d05119db0781f965a0bc
arpan <me@arpanrec.com>
2022-02-08 19:40:22 +0530

    pki_manage

---
+++ repo-pki-manage.tf
@@ -0,0 +1,13 @@
+resource "github_repository" "pki_manage" {
+  name               = "pki-manage"
+  license_template   = var.license_template
+  description        = "Manage PKI"
+  visibility         = "public"
+  gitignore_template = "Python"
+  auto_init          = true
+}
+
+resource "github_branch_default" "ca_manage" {
+  repository = github_repository.pki_manage.name
+  branch     = var.repository_default_branch_01292022_main
+}
