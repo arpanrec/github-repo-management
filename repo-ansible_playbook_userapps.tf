ba10e4d23ca54a787150e1a3451d10b145dd56d9
arpan m <me@arpanrec.com>
2022-02-16 02:26:50 +0530

    Create repo-ansible_playbook_userapps.tf

---
+++ repo-ansible_playbook_userapps.tf
@@ -0,0 +1,13 @@
+resource "github_repository" "ansible_playbook_userapps" {
+  name               = "ansible-playbook-userapps"
+  description        = "Install Apps in user dir"
+  visibility         = "public"
+  license_template   = var.license_template
+  auto_init          = true
+  gitignore_template = "Python"
+}
+
+resource "github_branch_default" "ansible_playbook_userapps" {
+  repository = github_repository.ansible_playbook_userapps.name
+  branch     = var.repository_default_branch_01062022_main
+}
