2447fd46fc79703a2047fbf050e43bee00e5b1ca
arpan <me@arpanrec.com>
2022-02-23 17:55:04 +0530

    code_style

---
+++ repo-code-style.tf
@@ -0,0 +1,11 @@
+resource "github_repository" "code_style" {
+  name               = "code-style"
+  license_template   = var.license_template
+  description        = "Coding Guidelines"
+  visibility         = "public"
+  auto_init          = true
+}
+resource "github_branch_default" "code_style" {
+  repository = github_repository.code_style.name
+  branch     = var.repository_default_branch_01292022_main
+}
