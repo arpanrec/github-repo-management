62814bf565a9e2a79de6e5bfb5ea57c178ae3950
arpan <me@arpanrec.com>
2022-02-24 21:10:09 +0530

    fmt

--- repo-code-style.tf
+++ repo-code-style.tf
@@ -1,9 +1,9 @@
 resource "github_repository" "code_style" {
-  name               = "code-style"
-  license_template   = var.license_template
-  description        = "Coding Guidelines"
-  visibility         = "public"
-  auto_init          = true
+  name             = "code-style"
+  license_template = var.license_template
+  description      = "Coding Guidelines"
+  visibility       = "public"
+  auto_init        = true
 }
 resource "github_branch_default" "code_style" {
   repository = github_repository.code_style.name
