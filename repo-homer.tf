ba647bab326cea6ab9366e2906802edbc7d71a8a
arpan m <me@arpanrec.com>
2022-07-24 13:26:27 +0530

    NS-39 homer

---
+++ repo-homer.tf
@@ -0,0 +1,13 @@
+resource "github_repository" "homer" {
+  name = "homer"
+  description        = "Home Page"
+  visibility         = "public"
+  license_template   = var.license_template
+  auto_init          = true
+  gitignore_template = "Node"
+}
+
+resource "github_branch_default" "homer" {
+  repository = github_repository.homer.name
+  branch     = var.repository_default_branch_01292022_main
+}
